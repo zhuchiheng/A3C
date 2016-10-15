899897c5e5307917e4be720617d5a5c8473d5d41
He-chien Tsai <depot051@gmail.com>
2016-10-16 02:06:06 +0800

    implement optimization for custom gradients with some workaround.

--- a3c.py
+++ a3c.py
@@ -1,6 +1,15 @@
 import keras.backend as K
 import numpy as np
 from keras.models import Model
+import keras.optimizers as opts
+
+
+def to_list(a):
+    return a if type(a) is list else [a]
+
+
+def de_list(a):
+    return a[0] if len(a) == 1 else a
 
 
 class A3C:
@@ -8,6 +17,29 @@ class A3C:
         self.p = p
         self.v = v
         self.t = 0
+        self.p_fake_x = de_list([np.zeros(s)
+                                 for s in to_list(self.p.input_shape)])
+        self.v_fake_x = de_list([np.zeros(s)
+                                 for s in to_list(self.v.input_shape)])
+        self.p_fake_y = de_list([np.zeros(s)
+                                 for s in to_list(self.p.output_shape)])
+        self.v_fake_y = de_list([np.zeros(s)
+                                 for s in to_list(self.v.output_shape)])
+
+    def compile(self, optimizer_p, optimizer_v):
+        opt_p = opts.get(optimizer_p)
+        opt_v = opts.get(optimizer_v)
+
+        def get_gradients(s, _1, _2):
+            return s.grads
+
+        def loss(_1, _2):
+            return 0
+
+        opt_p.get_gradients = get_gradients
+        opt_v.get_gradients = get_gradients
+        self.p.compile(optimizer=opt_p, loss=loss)
+        self.v.compile(optimizer=opt_v, loss=loss)
 
     def thread_step(self, env, gm=0.9, t_max=np.inf):
         d_th_p = [np.zeros(w.shape)
@@ -53,5 +85,9 @@ class A3C:
 
             self.async_update(d_th_p, d_th_v)
 
-    def async_update(d_th_p, d_th_v):
-        raise NotImplementedError
+    def async_update(self, d_th_p, d_th_v):
+        self.p.optimizer.grads = d_th_p
+        self.v.optimizer.grads = d_th_v
+        self.p.train_on_batch(self.p_fake_x, self.p_fake_y)
+        self.v.train_on_batch(self.v_fake_x, self.v_fake_y)
+
