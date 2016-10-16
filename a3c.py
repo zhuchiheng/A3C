fix gradients expression for optimizer

a3c.py
@@ -2,6 +2,7 @@ import keras.backend as K
 import numpy as np
 from keras.models import Model
 import keras.optimizers as opts
+import threading as z
 
 
 def to_list(a):
@@ -25,6 +26,7 @@ class A3C:
                                  for s in to_list(self.p.output_shape)])
         self.v_fake_y = de_list([np.zeros(s)
                                  for s in to_list(self.v.output_shape)])
+        self.lock = z.Lock()
 
     def compile(self, optimizer_p, optimizer_v):
         opt_p = opts.get(optimizer_p)
@@ -67,27 +69,24 @@ class A3C:
 
         x = K.placeholder(shape=(1,))
         dd_p = [
-            K.function(
-                p.inputs + [x],
-                [K.gradients(K.sum(K.log(aa) * x), w)
-                 for aa in p.outputs]
-            ) for w in p.trainable_weights]
+            [K.gradients(K.sum(K.log(aa)), w)
+             for aa in p.outputs]
+            for w in p.trainable_weights]
         dd_v = [
-            K.function(
-                v.inputs + [x],
-                [K.gradients((x - v.outputs[0])**2, w)]
-            ) for w in p.trainable_weights]
+            K.gradients((x - v.outputs[0])**2, w)
+            for w in p.trainable_weights]
         for s, a, r in h:
             R = r + gm * R
-            d_th_p = [w + sum(dd(R - v.predict(s)))
+            RR = R - v.predict(s)
+            d_th_p = [w + sum(dd) * RR
                       for dd, w in zip(dd_p, d_th_p)]
-            d_th_v = [w + dd_v(R)[0] for w in d_th_v]
+            d_th_v = [w + dd_v * R for w in d_th_v]
 
-            self.async_update(d_th_p, d_th_v)
-
-    def async_update(self, d_th_p, d_th_v):
         self.p.optimizer.grads = d_th_p
         self.v.optimizer.grads = d_th_v
-        self.p.train_on_batch(self.p_fake_x, self.p_fake_y)
-        self.v.train_on_batch(self.v_fake_x, self.v_fake_y)
+        p.train_on_batch(self.p_fake_x, self.p_fake_y)
+        v.train_on_batch(self.v_fake_x, self.v_fake_y)
 
+        with self.lock:
+            self.p.set_weights(p.get_weights())
+            self.v.set_weights(v.get_weights())